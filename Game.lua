d9963744ead0694a850c675f1a02bd22920c0fcd
Hadi Sebastian Santamaria Meza <108088079+4leph0mega@users.noreply.github.com>
2025-08-07 19:45:12 -0600

    Update Game.lua

--- Game.lua
+++ Game.lua
@@ -1,8 +1,9 @@
--- Create a borderless window at (200,200), size 400x300
-win = Window.new("Lua Window", 200, 200, 400, 300, true)
+local win = Window("Test", 100, 100, 800, 600, false)
+local tex = ImageBuffer()
+tex:loadFromFile("example.jpg")
+win:renderImageBuffer(tex)
+win:setVSync(true)
 
--- Set background color to teal
-win:set_color(0, 128, 128)
-
--- Clear (fill background)
-win:clear()
+function Update()
+    win:renderImageBuffer(tex)
+end
